c86997f7625b9d248b3081fde26402c3c194d44c
AleKobek <ola.kobek@gmail.com>
2026-03-30 12:24:23 +0200

    feat: dodanie service do wspieranej gry

---
+++ Squadra.Server/Modules/WspieraneGry/Services/IWspieranaGraService.cs
@@ -0,0 +1,15 @@
+﻿using Squadra.Server.Modules.Platformy.Models;
+using Squadra.Server.Modules.Shared.Services;
+using Squadra.Server.Modules.WspieraneGry.DTO;
+using Squadra.Server.Modules.WspieraneGry.Models;
+
+namespace Squadra.Server.Modules.WspieraneGry.Services;
+
+public interface IWspieranaGraService
+{
+    public Task<ServiceResult<ICollection<WspieranaGra>>> GetWspieraneGry();
+    public Task<ServiceResult<WspieranaGra>> GetWspieranaGra(int idGry);
+    public Task<ServiceResult<ICollection<WspieranaGra>>> GetWspieraneGryMinInfo();
+    public Task<ServiceResult<ICollection<GraZPlatformaDTO>>> GetWspieraneGryZPlatformami();
+    public Task<ServiceResult<ICollection<Platforma>>> GetPlatformyGry(int idGry);
+}
---
+++ Squadra.Server/Modules/WspieraneGry/Services/WspieranaGraService.cs
@@ -0,0 +1,54 @@
+﻿using Squadra.Server.Exceptions;
+using Squadra.Server.Modules.Platformy.Models;
+using Squadra.Server.Modules.Shared.Services;
+using Squadra.Server.Modules.WspieraneGry.DTO;
+using Squadra.Server.Modules.WspieraneGry.Models;
+using Squadra.Server.Modules.WspieraneGry.Repositories;
+
+namespace Squadra.Server.Modules.WspieraneGry.Services;
+
+public class WspieranaGraService(IWspieranaGraRepository wspieranaGraRepository) : IWspieranaGraService
+{
+    public async Task<ServiceResult<ICollection<WspieranaGra>>> GetWspieraneGry()
+    {
+        return ServiceResult<ICollection<WspieranaGra>>.Ok(await wspieranaGraRepository.GetWspieraneGry());
+    }
+
+    public async Task<ServiceResult<WspieranaGra>> GetWspieranaGra(int idGry)
+    {
+        try
+        {
+            if (idGry <= 0)
+                return ServiceResult<WspieranaGra>.NotFound(new ErrorItem("Gra o id " + idGry + " nie istnieje."));
+            return ServiceResult<WspieranaGra>.Ok(await wspieranaGraRepository.GetWspieranaGra(idGry));
+        }
+        catch (NieZnalezionoWBazieException e)
+        {
+            return ServiceResult<WspieranaGra>.NotFound(new ErrorItem(e.Message));
+        }
+    }
+    
+    public async Task<ServiceResult<ICollection<WspieranaGra>>> GetWspieraneGryMinInfo()
+    {
+        return ServiceResult<ICollection<WspieranaGra>>.Ok(await wspieranaGraRepository.GetWspieraneGryMinInfo());
+    }
+    
+    public async Task<ServiceResult<ICollection<GraZPlatformaDTO>>> GetWspieraneGryZPlatformami()
+    {
+        return ServiceResult<ICollection<GraZPlatformaDTO>>.Ok(await wspieranaGraRepository.GetWspieraneGryZPlatformami());
+    }
+    
+    public async Task<ServiceResult<ICollection<Platforma>>> GetPlatformyGry(int idGry)
+    {
+        try
+        {
+            if (idGry <= 0)
+                return ServiceResult<ICollection<Platforma>>.NotFound(new ErrorItem("Gra o id " + idGry + " nie istnieje."));
+            return ServiceResult<ICollection<Platforma>>.Ok(await wspieranaGraRepository.GetPlatformyGry(idGry));
+        }
+        catch (NieZnalezionoWBazieException e)
+        {
+            return ServiceResult<ICollection<Platforma>>.NotFound(new ErrorItem(e.Message));
+        }
+    }
+}
