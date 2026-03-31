b6cb9fad2864486f72d8733002e839ec513883bf
AleKobek <ola.kobek@gmail.com>
2026-03-31 14:13:55 +0200

    feat: dodanie service biblioteki gier, potem się podepnie statystyki gdy będą istnieć

---
+++ Squadra.Server/Modules/BibliotekaGier/Services/BibliotekaGierService.cs
@@ -0,0 +1,15 @@
+﻿using Squadra.Server.Modules.BibliotekaGier.DTO;
+using Squadra.Server.Modules.BibliotekaGier.Repositories;
+using Squadra.Server.Modules.Shared.Services;
+
+namespace Squadra.Server.Modules.BibliotekaGier.Services;
+
+public class BibliotekaGierService(IBibliotekaGierRepository bibliotekaGierRepository) : IBibliotekaGierService
+{
+    public async Task<ServiceResult<ICollection<GraWBiblioteceDTO>>> PodajGryWBiblioteceUzytkownika(int idUzytkownika)
+    {
+        var listaGier = await bibliotekaGierRepository.PodajGryWBiblioteceUzytkownika(idUzytkownika);
+        // tutaj będzie podmiana 0 w godzinach rozgrywki na prawdziwe statystyki, ale to gdy zrobię moduł statystyk
+        return ServiceResult<ICollection<GraWBiblioteceDTO>>.Ok(listaGier);
+    }
+}
---
+++ Squadra.Server/Modules/BibliotekaGier/Services/IBibliotekaGierService.cs
@@ -0,0 +1,10 @@
+﻿using Squadra.Server.Modules.BibliotekaGier.DTO;
+using Squadra.Server.Modules.Shared.Services;
+
+namespace Squadra.Server.Modules.BibliotekaGier.Services;
+
+public interface IBibliotekaGierService
+{
+    public Task<ServiceResult<ICollection<GraWBiblioteceDTO>>> PodajGryWBiblioteceUzytkownika(int idUzytkownika);
+
+}
