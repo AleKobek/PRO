2508b122a7ef3d60a94a8e29fa0b66c4cd00f6ed
AleKobek <ola.kobek@gmail.com>
2026-04-01 16:58:41 +0200

    feat: dodanie serwisu statystyk

---
+++ Squadra.Server/Modules/Statystyki/Services/IStatystykiService.cs
@@ -0,0 +1,11 @@
+﻿using Squadra.Server.Modules.Shared.Services;
+using Squadra.Server.Modules.Statystyki.DTO;
+
+namespace Squadra.Server.Modules.Statystyki.Services;
+
+public interface IStatystykiService
+{
+    public Task<ServiceResult<string>> GetGodzinyGrania(int idUzytkownika, int idGry);
+    public Task<ServiceResult<string?>> GetWartoscStatystyki(int idUzytkownika, int idStatystyki);
+    public Task<ServiceResult<ICollection<StatystykaDTO>>> GetStatystykiZGry(int idUzytkownika, int idGry);
+}
---
+++ Squadra.Server/Modules/Statystyki/Services/StatystykiService.cs
@@ -0,0 +1,75 @@
+﻿using Squadra.Server.Exceptions;
+using Squadra.Server.Modules.Shared.Services;
+using Squadra.Server.Modules.Statystyki.DTO;
+using Squadra.Server.Modules.Statystyki.Repositories;
+
+namespace Squadra.Server.Modules.Statystyki.Services;
+
+public class StatystykiService(IStatystykiRepository statystykiRepository) : IStatystykiService
+{
+    public async Task<ServiceResult<string>> GetGodzinyGrania(int idUzytkownika, int idGry)
+    {
+        if (idUzytkownika <= 0)
+        {
+            return ServiceResult<string>.BadRequest(new ErrorItem("Nieprawidłowy identyfikator użytkownika: " + idUzytkownika));
+        }
+        if (idGry <= 0)
+        {
+            return ServiceResult<string>.BadRequest(new ErrorItem("Nieprawidłowy identyfikator gry: " + idGry));
+        }
+        
+        try
+        {
+            var result = await statystykiRepository.GetGodzinyGrania(idUzytkownika, idGry);
+            return ServiceResult<string>.Ok(result);
+        }
+        catch (NieZnalezionoWBazieException ex)
+        {
+            return ServiceResult<string>.NotFound(new ErrorItem(ex.Message));
+        }
+    }
+    
+    public async Task<ServiceResult<string?>> GetWartoscStatystyki(int idUzytkownika, int idStatystyki)
+    {
+        if (idUzytkownika <= 0)
+        {
+            return ServiceResult<string?>.BadRequest(new ErrorItem("Nieprawidłowy identyfikator użytkownika: " + idUzytkownika));
+        }
+        if (idStatystyki <= 0)
+        {
+            return ServiceResult<string?>.BadRequest(new ErrorItem("Nieprawidłowy identyfikator statystyki: " + idStatystyki));
+        }
+        
+        try
+        {
+            var result = await statystykiRepository.GetWartoscStatystyki(idUzytkownika, idStatystyki);
+            return ServiceResult<string?>.Ok(result);
+        }
+        catch (NieZnalezionoWBazieException ex)
+        {
+            return ServiceResult<string?>.NotFound(new ErrorItem(ex.Message));
+        }
+    }
+    
+    public async Task<ServiceResult<ICollection<StatystykaDTO>>> GetStatystykiZGry(int idUzytkownika, int idGry)
+    {
+        if (idUzytkownika <= 0)
+        {
+            return ServiceResult<ICollection<StatystykaDTO>>.BadRequest(new ErrorItem("Nieprawidłowy identyfikator użytkownika: " + idUzytkownika));
+        }
+        if (idGry <= 0)
+        {
+            return ServiceResult<ICollection<StatystykaDTO>>.BadRequest(new ErrorItem("Nieprawidłowy identyfikator gry: " + idGry));
+        }
+        
+        try
+        {
+            var result = await statystykiRepository.GetStatystykiZGry(idUzytkownika, idGry);
+            return ServiceResult<ICollection<StatystykaDTO>>.Ok(result);
+        }
+        catch (NieZnalezionoWBazieException ex)
+        {
+            return ServiceResult<ICollection<StatystykaDTO>>.NotFound(new ErrorItem(ex.Message));
+        }
+    }
+}
