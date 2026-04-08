74ca0ab4b8b96f4cef2287d98082bacf8291d690
AleKobek <ola.kobek@gmail.com>
2026-04-08 19:21:31 +0200

    feat: dodanie UpdatePlatformyUzytkownika do interfejsu repozytorium platform

--- Squadra.Server/Modules/Platformy/Repositories/IPlatformaRepository.cs
+++ Squadra.Server/Modules/Platformy/Repositories/IPlatformaRepository.cs
@@ -8,5 +8,6 @@ public interface IPlatformaRepository
     public Task<ICollection<Platforma>> GetPlatformy();
     public Task<Platforma> GetPlatforma(int id);
     public Task<ICollection<PlatformaUzytkownikaDTO>> GetPlatformyUzytkownika(int idUzytkownika);
+    public Task<ICollection<PlatformaUzytkownikaDTO>> UpdatePlatformyUzytkownika(int idUzytkownika);
     public Task<bool> UsunPlatformyUzytkownika(int idUzytkownika);
 }
