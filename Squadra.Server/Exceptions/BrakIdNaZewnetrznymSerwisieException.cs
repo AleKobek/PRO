3d29ca0cbaaa90ff48722bcb8081ff5ce13217cf
AleKobek <ola.kobek@gmail.com>
2026-04-08 18:40:20 +0200

    feat: dodanie nowego wyjątku, który będzie rzucany w przypadku braku id na zewnętrznym serwisie

---
+++ Squadra.Server/Exceptions/BrakIdNaZewnetrznymSerwisieException.cs
@@ -0,0 +1,15 @@
+﻿namespace Squadra.Server.Exceptions;
+
+public class BrakIdNaZewnetrznymSerwisieException : Exception
+{
+    public BrakIdNaZewnetrznymSerwisieException ()
+    {}
+
+    public BrakIdNaZewnetrznymSerwisieException (string message) 
+        : base(message)
+    {}
+
+    public BrakIdNaZewnetrznymSerwisieException (string message, Exception innerException)
+        : base (message, innerException)
+    {}
+}
