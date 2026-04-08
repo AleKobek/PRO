be9cb06e980959fb45b6466a0dbb87a83f254361
AleKobek <ola.kobek@gmail.com>
2026-04-08 19:39:08 +0200

    feat: dodanie do statystyki użytkownika kolumny "porównywalna wartość liczbowa"

---
+++ Squadra.Server/Migrations/20260408173825_Dodanie_kolumny_porownywalna_wartosc_liczbowa_do_statystyki_uzytkownika.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Squadra.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Dodanie_kolumny_porownywalna_wartosc_liczbowa_do_statystyki_uzytkownika : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PorownywalnaWartoscLiczbowa",
+                table: "StatystykaUzytkownika",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PorownywalnaWartoscLiczbowa",
+                table: "StatystykaUzytkownika");
+        }
+    }
+}
--- Squadra.Server/Modules/Statystyki/Models/StatystykaUzytkownika.cs
+++ Squadra.Server/Modules/Statystyki/Models/StatystykaUzytkownika.cs
@@ -7,6 +7,7 @@ public class StatystykaUzytkownika
     public int UzytkownikId { get; set; }
     public int StatystykaId { get; set; }
     public string Wartosc { get; set; }
+    public int? PorownywalnaWartoscLiczbowa { get; set; }
     
     public virtual Statystyka Statystyka { get; set; }
     public virtual Uzytkownik Uzytkownik { get; set; }
