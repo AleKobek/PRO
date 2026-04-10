0a3ddda012973e4e29c71fe9ee5f649f49bba06c
AleKobek <ola.kobek@gmail.com>
2026-04-10 15:06:49 +0200

    feat: uwzględnienie funkcji przerywającej integrację w interfejsie serwisu integracji zewnętrznych

--- Squadra.Server/Modules/IntegracjeZewnetrzne/Services/IIntegracjeZewnetrzneService.cs
+++ Squadra.Server/Modules/IntegracjeZewnetrzne/Services/IIntegracjeZewnetrzneService.cs
@@ -5,5 +5,6 @@ namespace Squadra.Server.Modules.IntegracjeZewnetrzne.Services;
 public interface IIntegracjeZewnetrzneService
 {
     public Task<ServiceResult<bool>> ZintegrujKonto(int idUzytkownika, string login, string haslo);
+    public Task<ServiceResult<bool>> PrzerwijIntegracjeUzytkownika(int idUzytkownika);
     public Task<ServiceResult<bool>> UpdateCaleZintegrowaneDaneUzytkownika(int idUzytkownika);
 }
