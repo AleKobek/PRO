20d7d6b2167ea9701d04a8b654d7a87ce4ed0057
AleKobek <ola.kobek@gmail.com>
2026-04-11 16:28:04 +0200

    fix: zwykły nagłówek teraz także ma ikonkę przy logo

--- squadra.client/src/Components/Naglowek.jsx
+++ squadra.client/src/Components/Naglowek.jsx
@@ -8,7 +8,10 @@ export default function Naglowek() {
             <title>Squadra</title>
         </header>
         <div className = "menu" id="menu">
-        <span className="logo">Squadra</span>
+            <div className="flex items-center flex-row  gap-3 h-full">
+                <span className="logo">Squadra</span>
+                <img src="/img/gamepad-2.svg" alt="gamepad" className="h-[clamp(28px,3vw,52px)] w-auto" />
+            </div>
         <ul id = "menu-na-pasku">
             <NavLink to = '/' className={({isActive}) => isActive ? 'nawigacja active' : 'nawigacja'}>Strona główna</NavLink>
             <NavLink to = '/login' className={({isActive}) => isActive ? 'nawigacja active' : 'nawigacja'}>Zaloguj się</NavLink>
