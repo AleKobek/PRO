8a37c16072817b53cc6ca1a3be91756d39a3e653
AleKobek <ola.kobek@gmail.com>
2026-05-02 19:02:42 +0200

    feat: dodanie repozytoriów do modułu drużyn

---
+++ Squadra.Server/Modules/Drużyny/Repositories/DruzynyRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Squadra.Server.Context;
+using Squadra.Server.Exceptions;
+using Squadra.Server.Modules.Drużyny.Models;
+
+namespace Squadra.Server.Modules.Drużyny.Repositories;
+
+public class DruzynyRepository(AppDbContext context) : IDruzynyRepository
+{
+    
+    public async Task<Druzyna> GetDruzyna(int idDruzyny)
+    {
+        var druzyna = await context.Druzyna.FindAsync(idDruzyny);
+        if (druzyna == null) throw new NieZnalezionoWBazieException("Nie znaleziono drużyny o id " + idDruzyny);
+        return druzyna;
+    }
+    
+    public async Task<ICollection<MiejsceWDruzynie>> GetMiejscaWDruzynie(int idDruzyny)
+    {
+        var druzyna = await context.Druzyna.FindAsync(idDruzyny);
+        if (druzyna == null) throw new NieZnalezionoWBazieException("Nie znaleziono drużyny o id " + idDruzyny);
+        
+        var miejscaWDruzynie = await context.MiejsceWDruzynie
+            .Where(m => m.DruzynaId == idDruzyny)
+            .Include(m => m.Rola)
+            .ToListAsync();
+
+        return miejscaWDruzynie;
+    }
+    
+    public async Task<NastrojRozgrywki> GetNastrojRozgrywki(int idNastroju)
+    {
+        var nastroj = await context.NastrojRozgrywki.FindAsync(idNastroju);
+        if (nastroj == null) throw new NieZnalezionoWBazieException("Nie znaleziono nastroju o id " + idNastroju);
+        return nastroj;
+    }
+}
---
+++ Squadra.Server/Modules/Drużyny/Repositories/IDruzynyRepository.cs
@@ -0,0 +1,10 @@
+﻿using Squadra.Server.Modules.Drużyny.Models;
+
+namespace Squadra.Server.Modules.Drużyny.Repositories;
+
+public interface IDruzynyRepository
+{
+    public Task<Druzyna> GetDruzyna(int idDruzyny);
+    public Task<ICollection<MiejsceWDruzynie>> GetMiejscaWDruzynie(int idDruzyny);
+    public Task<NastrojRozgrywki> GetNastrojRozgrywki(int idNastroju);
+}
