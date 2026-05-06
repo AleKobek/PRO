9455438db3ae5db34f00c3c6383d21858eebd135
AleKobek <ola.kobek@gmail.com>
2026-05-06 13:46:51 +0200

    feat: dodanie do kontrolera drużyn funkcji zwracających dane potrzebne do obu typów formularza

--- Squadra.Server/Modules/Drużyny/Controllers/DruzynaController.cs
+++ Squadra.Server/Modules/Drużyny/Controllers/DruzynaController.cs
@@ -43,5 +43,40 @@ public class DruzynaController(IDruzynyService druzynyService) : ControllerBase
             404 => NotFound(result.Errors[0].Message),
             _ => StatusCode(result.StatusCode, new { errors = result.Errors })
         };
-     }
+    }
+    [HttpGet("formularz/ze-statystykami/{idUzytkownika:int}/{idGry:int}")]
+    [EndpointSummary("Zwraca dane potrzebne do wyświetlenia formularza tworzenia drużyny, ze statystykami.")]
+    [EndpointDescription("Podajemy uzytkownika, który tworzy drużynę, aby zwrócić jego statystyki, które mogą być przydatne przy tworzeniu drużyny. Podajemy też id gry, aby zwrócić tylko statystyki z tej gry.")]
+    [ProducesResponseType(typeof(DaneDoFormularzaDruzynyZeStatystykamiDto), 200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
+    public async Task<ActionResult> GetDaneDoFormularzaDruzynyZeStatystykami(int idUzytkownika, int idGry)
+    {
+        var result = await druzynyService.GetDaneDoFormularzaDruzynyZeStatystykami(idGry, idUzytkownika);
+        return result.StatusCode switch
+        {
+            200 => Ok(result.Value),
+            400 => BadRequest(result.Errors[0].Message),
+            404 => NotFound(result.Errors[0].Message),
+            _ => StatusCode(result.StatusCode, new { errors = result.Errors })
+        };
+    }
+    
+     [HttpGet("formularz/bez-statystyk/{idGry:int}")]
+     [EndpointSummary("Zwraca dane potrzebne do wyświetlenia formularza tworzenia drużyny, bez statystyk.")]
+     [EndpointDescription("Zwraca dane potrzebne do wyświetlenia formularza tworzenia drużyny, bez statystyk. Podajemy id gry, aby zwrócić tylko statystyki z tej gry.")]
+     [ProducesResponseType(typeof(DaneDoFormularzaDruzynyBezStatystykDto), 200)]
+     [ProducesResponseType(400)]
+     [ProducesResponseType(404)]
+     public async Task<ActionResult> GetDaneDoFormularzaDruzynyBezStatystyk(int idGry)
+    {
+        var result = await druzynyService.GetDaneDoFormularzaDruzynyBezStatystyk(idGry);
+        return result.StatusCode switch
+        {
+            200 => Ok(result.Value),
+            400 => BadRequest(result.Errors[0].Message),
+            404 => NotFound(result.Errors[0].Message),
+            _ => StatusCode(result.StatusCode, new { errors = result.Errors })
+        };
+    }
 }
