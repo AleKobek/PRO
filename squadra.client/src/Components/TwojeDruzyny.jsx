e75b52856034bacf214689785e86cdd24d213c4b
AleKobek <ola.kobek@gmail.com>
2026-05-07 15:59:38 +0200

    refactor: usunięcie (chyba) zbędnego toast container na stronie twoich drużyn

--- squadra.client/src/Components/TwojeDruzyny.jsx
+++ squadra.client/src/Components/TwojeDruzyny.jsx
@@ -34,18 +34,5 @@ export default function TwojeDruzyny() {
                 <TabelkaTwoichDruzynKomponent idUzytkownika={uzytkownik.id}/>
             </div>
         </div>
-        <ToastContainer
-            position="top-center"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick={false}
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-            transition={Bounce}
-        />
     </>);
 }
