48bac56ca2d82716e7725cc3fed04f5682fbab10
AleKobek <ola.kobek@gmail.com>
2026-05-12 19:49:15 +0200

    refactor: create miejsce w drużynie dto nie potrzebuje już id drużyny i użytkownika
    używamy tego tylko przy tworzeniu drużyny

--- Squadra.Server/Modules/Drużyny/DTO/CreateMiejsceWDruzynieReq.cs
+++ Squadra.Server/Modules/Drużyny/DTO/CreateMiejsceWDruzynieReq.cs
@@ -3,8 +3,6 @@
 namespace Squadra.Server.Modules.Drużyny.DTO;
 
 public record CreateMiejsceWDruzynieReq(
-    int IdDruzyny,
-    int? IdUzytkownika,
     int? IdRoli,
     WartoscStatystykiDTO? WymaganaStatystyka
 );
