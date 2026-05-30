5e0d8b935e7d2402c1f9a07d998c48dfc9abbf48
AleKobek <ola.kobek@gmail.com>
2026-05-30 15:51:26 +0200

    fix: nadrobienie dodania do git migracji usuwającej kolumnę "czy 18+"

---
+++ Squadra.Server/Migrations/20260529150905_Usuniecie_kolumny_czy_18_plus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Squadra.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Usuniecie_kolumny_czy_18_plus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "czy_18_plus",
+                table: "Druzyna");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "czy_18_plus",
+                table: "Druzyna",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
