6ef881b455059804da3ab273532395eaa55547f8
AleKobek <ola.kobek@gmail.com>
2026-06-11 15:55:49 +0200

    refactor(klient): dodanie komentarzy do enuma, objaśniających budowę powiadomień o danych typach

--- Squadra.Server/Modules/Powiadomienia/Enums/TypPowiadomieniaEnum.cs
+++ Squadra.Server/Modules/Powiadomienia/Enums/TypPowiadomieniaEnum.cs
@@ -2,14 +2,14 @@
 
 public enum TypPowiadomieniaEnum
 {
-    Systemowe = 1,
-    ZaproszenieDoZnajomych = 2,
-    PrzyjecieZaproszeniaDoZnajomych = 3,
-    OdrzucenieZaproszeniaDoZnajomych = 4,
-    UsuniecieZnajomosci = 5,
-    UzytkownikDolaczylDoDruzyny = 6,
+    Systemowe = 1, // brak powiązanych obiektów, tylko treść powiadomienia
+    ZaproszenieDoZnajomych = 2, // jedynym powiązanym obiektem jest nadawca.
+    PrzyjecieZaproszeniaDoZnajomych = 3, // jedynym powiązanym obiektem jest drugi użytkownik.
+    OdrzucenieZaproszeniaDoZnajomych = 4, // jedynym powiązanym obiektem jest drugi użytkownik.
+    UsuniecieZnajomosci = 5, // jedynym powiązanym obiektem jest drugi użytkownik.
+    UzytkownikDolaczylDoDruzyny = 6, // pierwszym powiązanym obiektem jest użytkownik, drugim powiązanym obiektem jest drużyna. W treści jest rola miejsca
     UsuniecieZDruzyny = 7,
-    ZaproszenieDoDruzyny = 8,
+    ZaproszenieDoDruzyny = 8, // pierwszym powiązanym obiektem jest drużyna, drugim jest miejsce (bez nazwy). W treści jest rola miejsca
     PrzyjecieZaproszeniaDoDruzyny = 9,
     OdrzucenieZaproszeniaDoDruzyny = 10
 }
