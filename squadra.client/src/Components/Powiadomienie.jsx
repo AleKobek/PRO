a1a94113cbad9e76599c810ddd22dac19d94c5b1
AleKobek <ola.kobek@gmail.com>
2026-06-11 19:48:48 +0200

    feat(klient): prawidłowe wyświetlanie dodanych typów powiadomień
    użytkownik dołączył do drużyny, opuścił drużynę, usunięto cię z drużyny, zaproszono cię do drużyny

--- squadra.client/src/Components/Powiadomienie.jsx
+++ squadra.client/src/Components/Powiadomienie.jsx
@@ -33,7 +33,10 @@ export default function Powiadomienie({powiadomienie, przyRozpatrzaniuPowiadomie
         ZAAKCEPTOWANIE_ZAPROSZENIA_DO_ZNAJOMYCH: 3,
         ODRZUCENIE_ZAPROSZENIA_DO_ZNAJOMYCH: 4,
         USUNIETO_CIE_ZE_ZNAJOMYCH: 5,
-        UZYTKOWNIK_DOLACZYL_DO_DRUZYNY: 6
+        UZYTKOWNIK_DOLACZYL_DO_DRUZYNY: 6,
+        USUNIETO_CIE_Z_DRUZYNY: 7,
+        ZAPROSZENIE_DO_DRUZYNY: 8,
+        UZYTKOWNIK_OPUSCIL_DRUZYNE: 13
     }),[]);
 
     // ustawiamy treść powiadomienia na podstawie typu powiadomienia
@@ -76,9 +79,29 @@ export default function Powiadomienie({powiadomienie, przyRozpatrzaniuPowiadomie
                 ustawTrescPowiadomieniaCz1("Uzytkownik ");
                 ustawTrescPowiadomieniaCz2(" dołączył do Twojej drużyny: ");
                 if(powiadomienie.tresc?.length > 0){
-                    ustawTrescPowiadomieniaCz3(" jako: " + powiadomienie.tresc + "."); // tu będzie rola
+                    ustawTrescPowiadomieniaCz3(" jako " + powiadomienie.tresc + "."); // tu będzie rola
                 } else {
                     ustawTrescPowiadomieniaCz3(".");
+                }
+                break;
+            }
+            case TypyPowiadomien.USUNIETO_CIE_Z_DRUZYNY:{
+                ustawTypPowiadomienia("Usunięto cię z drużyny");
+                ustawTrescPowiadomieniaCz1("Usunięto cię z drużyny: ");
+                break;
+            }
+            case TypyPowiadomien.ZAPROSZENIE_DO_DRUZYNY:{
+                ustawTypPowiadomienia("Zaproszenie do drużyny");
+                ustawTrescPowiadomieniaCz1("Masz zaproszenie do drużyny: ");
+                break;
+            }
+            case TypyPowiadomien.UZYTKOWNIK_OPUSCIL_DRUZYNE:{
+                ustawTypPowiadomienia("Użytkownik opuścił drużynę")
+                ustawTrescPowiadomieniaCz1("Uzytkownik ");
+                if(powiadomienie.tresc?.length > 0){
+                    ustawTrescPowiadomieniaCz2("("+ powiadomienie.tresc +") opuścił Twoją drużynę: ");
+                }else {
+                    ustawTrescPowiadomieniaCz2(" opuścił Twoją drużynę: ");
                 }
                 break;
             }
@@ -87,7 +110,7 @@ export default function Powiadomienie({powiadomienie, przyRozpatrzaniuPowiadomie
             }
         }
 
-    }, [TypyPowiadomien.SYSTEMOWE, TypyPowiadomien.ZAPROSZENIE_DO_ZNAJOMYCH, TypyPowiadomien.ZAAKCEPTOWANIE_ZAPROSZENIA_DO_ZNAJOMYCH, TypyPowiadomien.ODRZUCENIE_ZAPROSZENIA_DO_ZNAJOMYCH, TypyPowiadomien.USUNIETO_CIE_ZE_ZNAJOMYCH, powiadomienie.idTypuPowiadomienia, powiadomienie.nazwaPowiazanegoObiektu, powiadomienie.tresc, powiadomienie, TypyPowiadomien.UZYTKOWNIK_DOLACZYL_DO_DRUZYNY]);
+    }, [TypyPowiadomien.SYSTEMOWE, TypyPowiadomien.ZAPROSZENIE_DO_ZNAJOMYCH, TypyPowiadomien.ZAAKCEPTOWANIE_ZAPROSZENIA_DO_ZNAJOMYCH, TypyPowiadomien.ODRZUCENIE_ZAPROSZENIA_DO_ZNAJOMYCH, TypyPowiadomien.USUNIETO_CIE_ZE_ZNAJOMYCH, powiadomienie.idTypuPowiadomienia, powiadomienie.nazwaPowiazanegoObiektu, powiadomienie.tresc, powiadomienie, TypyPowiadomien.UZYTKOWNIK_DOLACZYL_DO_DRUZYNY, TypyPowiadomien.USUNIETO_CIE_Z_DRUZYNY, TypyPowiadomien.ZAPROSZENIE_DO_DRUZYNY, TypyPowiadomien.UZYTKOWNIK_OPUSCIL_DRUZYNE]);
 
 
     if(!powiadomienie) return (<></>);
@@ -110,32 +133,40 @@ export default function Powiadomienie({powiadomienie, przyRozpatrzaniuPowiadomie
         <div className="text-xs text-gray-400 mt-1.5">{powiadomienie.dataWyslania}</div>
     </li>);
 
-    if(powiadomienie.idTypuPowiadomienia === TypyPowiadomien.ZAPROSZENIE_DO_ZNAJOMYCH)
+    if(powiadomienie.idTypuPowiadomienia === TypyPowiadomien.ZAPROSZENIE_DO_ZNAJOMYCH
+        || powiadomienie.idTypuPowiadomienia === TypyPowiadomien.ZAPROSZENIE_DO_DRUZYNY
+    ) {
+        let href = powiadomienie.idTypuPowiadomienia === TypyPowiadomien.ZAPROSZENIE_DO_ZNAJOMYCH
+            ? `${CLIENT_URL}/profil/` + powiadomienie.idPowiazanegoObiektu
+            : `${CLIENT_URL}/druzyna/` + powiadomienie.idPowiazanegoObiektu
+
         return (<li key={powiadomienie.id} className="p-2 border-b border-gray-200">
             <div className="font-semibold mb-1">{typPowiadomienia}</div>
             <div className="text-sm text-gray-600">
                 {trescPowiadomieniaCz1}
-                <a href={`${CLIENT_URL}/profil/`+powiadomienie.idPowiazanegoObiektu} className="text-black font-semibold">{powiadomienie.nazwaPowiazanegoObiektu}</a>
+                <a href={href} className="text-black font-semibold">{powiadomienie.nazwaPowiazanegoObiektu}</a>
                 {trescPowiadomieniaCz2}
-                {powiadomienie.idDrugiegoPowiazanegoObiektu &&
-                    <a href={`${CLIENT_URL}/profil/`+powiadomienie.idDrugiegoPowiazanegoObiektu} className="text-black font-semibold">{powiadomienie.nazwaDrugiegoPowiazanegoObiektu}</a>
-                }
-                {trescPowiadomieniaCz3}
             </div>
             <div className="text-xs text-gray-400 mt-1">{powiadomienie.dataWyslania}</div>
             {/* przycisk zaakceptuj i odrzuć */}
             <div className="flex justify-center items-center mt-2">
                 <button
                     className="text-white bg-green-900 rounded-lg px-3 py-2 mx-2 hover:bg-green-600"
-                    onClick={() => przyRozpatrzaniuPowiadomienia(powiadomienie.id, true)}>Zaakceptuj</button>
+                    onClick={() => przyRozpatrzaniuPowiadomienia(powiadomienie.id, true)}>Zaakceptuj
+                </button>
                 <button
                     className="text-white bg-red-900 rounded-lg px-3 py-2 mx-2 hover:bg-red-600"
-                    onClick={() => przyRozpatrzaniuPowiadomienia(powiadomienie.id, false)}>Odrzuć</button>
+                    onClick={() => przyRozpatrzaniuPowiadomienia(powiadomienie.id, false)}>Odrzuć
+                </button>
             </div>
         </li>);
+    }
 
+    // typy powiadomień: użytkownik dołączył do drużyny / opuścił drużynę
 
-    return (<li key={powiadomienie.id} className="p-2 border-b border-gray-200">
+    if(powiadomienie.idTypuPowiadomienia === TypyPowiadomien.UZYTKOWNIK_DOLACZYL_DO_DRUZYNY
+        || powiadomienie.idTypuPowiadomienia === TypyPowiadomien.UZYTKOWNIK_OPUSCIL_DRUZYNE)
+        return (<li key={powiadomienie.id} className="p-2 border-b border-gray-200">
         <div className="flex flex-row justify-between items-center w-full">
             <div className="font-semibold">{typPowiadomienia}</div>
             <button onClick={() => przyRozpatrzaniuPowiadomienia(powiadomienie.id, null)}>
@@ -151,9 +182,29 @@ export default function Powiadomienie({powiadomienie, przyRozpatrzaniuPowiadomie
             <a href={`${CLIENT_URL}/profil/`+powiadomienie.idPowiazanegoObiektu} className="text-black font-semibold">{powiadomienie.nazwaPowiazanegoObiektu}</a>
             {trescPowiadomieniaCz2}
             {powiadomienie.idDrugiegoPowiazanegoObiektu &&
-                <a href={`${CLIENT_URL}/profil/`+powiadomienie.idDrugiegoPowiazanegoObiektu} className="text-black font-semibold">{powiadomienie.nazwaDrugiegoPowiazanegoObiektu}</a>
+                <a href={`${CLIENT_URL}/druzyna/`+powiadomienie.idDrugiegoPowiazanegoObiektu} className="text-black font-semibold">{powiadomienie.nazwaDrugiegoPowiazanegoObiektu}</a>
             }
             {trescPowiadomieniaCz3}
+        </div>
+        <div className="text-xs text-gray-400 mt-1.5">{powiadomienie.dataWyslania}</div>
+    </li>)
+
+    // typy powiadomień: zaakceptowano / odrzucono zaproszenie do znajomych, usunięto cię ze znajomych
+    return (<li key={powiadomienie.id} className="p-2 border-b border-gray-200">
+        <div className="flex flex-row justify-between items-center w-full">
+            <div className="font-semibold">{typPowiadomienia}</div>
+            <button onClick={() => przyRozpatrzaniuPowiadomienia(powiadomienie.id, null)}>
+                <img
+                    src = "/img/x.svg"
+                    alt = "x"
+                    className="w-4 h-4 cursor-pointer"
+                />
+            </button>
+        </div>
+        <div className="text-sm text-gray-600">
+            {trescPowiadomieniaCz1}
+            <a href={`${CLIENT_URL}/profil/`+powiadomienie.idPowiazanegoObiektu} className="text-black font-semibold">{powiadomienie.nazwaPowiazanegoObiektu}</a>
+            {trescPowiadomieniaCz2}
         </div>
         <div className="text-xs text-gray-400 mt-1.5">{powiadomienie.dataWyslania}</div>
     </li>)
