9d89e8dc95594bc34035cc40c6e95c6efce3749b
AleKobek <ola.kobek@gmail.com>
2026-06-13 13:59:33 +0200

    feat(powiadomienia): dodanie do enuma typu powiadomienia "drużyna została rozwiązana"

--- Squadra.Server/Modules/Powiadomienia/Enums/TypPowiadomieniaEnum.cs
+++ Squadra.Server/Modules/Powiadomienia/Enums/TypPowiadomieniaEnum.cs
@@ -8,9 +8,10 @@ public enum TypPowiadomieniaEnum
     OdrzucenieZaproszeniaDoZnajomych = 4, // jedynym powiązanym obiektem jest drugi użytkownik.
     UsuniecieZnajomosci = 5, // jedynym powiązanym obiektem jest drugi użytkownik.
     UzytkownikDolaczylDoDruzyny = 6, // pierwszym powiązanym obiektem jest użytkownik, drugim powiązanym obiektem jest drużyna. W treści jest rola miejsca
-    UsuniecieZDruzyny = 7,
+    UsuniecieZDruzyny = 7, // jedynym powiazanym obiektem jest drużyna
     ZaproszenieDoDruzyny = 8, // pierwszym powiązanym obiektem jest drużyna, drugim jest miejsce (bez nazwy). W treści jest rola miejsca
-    PrzyjecieZaproszeniaDoDruzyny = 9,
-    OdrzucenieZaproszeniaDoDruzyny = 10,
-    UzytkownikOpuscilDruzyne = 13,
+    PrzyjecieZaproszeniaDoDruzyny = 9, // pierwszym obiektem jest użytkownik, drugim drużyna
+    OdrzucenieZaproszeniaDoDruzyny = 10, // pierwszym obiektem jest użytkownik, drugim drużyna
+    UzytkownikOpuscilDruzyne = 13, // pierwszym obiektem jest użytkownik, drugim drużyna
+    DruzynaZostalaRozwiazana = 14 // jedynym powiązanym obiektem jest drużyna, dokładnie to jej nazwa, bo sama drużyna już nie istnieje
 }
