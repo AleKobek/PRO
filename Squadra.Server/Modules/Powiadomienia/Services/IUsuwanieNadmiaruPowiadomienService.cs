1232aab25fd374a2bb8b8948989d7c1e03b04c68
AleKobek <ola.kobek@gmail.com>
2026-06-24 14:25:48 +0200

    refactor(powiadomienia): usunięcie UsuwanieNadmiaruPowiadomienService i przeniesienie jego funkcji do serwisu powiadomień
    i tak musimy tam importować drużyny repository w create powiadomienie, nie mam pomysłu jak nazwać serwis łączący usuwanie nadmiarowych powiadomień i tworzący powiadomienia

--- Squadra.Server/Modules/Powiadomienia/Services/IUsuwanieNadmiaruPowiadomienService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Squadra.Server.Modules.Powiadomienia.Services;
-
-public interface IUsuwanieNadmiaruPowiadomienService
-{
-    public Task<bool> UsunNadmiarowePowiadomieniaUzytkownika(int idUzytkownika);
-}
--- Squadra.Server/Modules/Powiadomienia/Services/PowiadomienieService.cs
+++ Squadra.Server/Modules/Powiadomienia/Services/PowiadomienieService.cs
@@ -1,6 +1,4 @@
-﻿using System.Security.Claims;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.IdentityModel.Tokens;
 using Squadra.Server.Exceptions;
 using Squadra.Server.Modules.Drużyny.Repositories;
 using Squadra.Server.Modules.Powiadomienia.DTO;
@@ -8,7 +6,7 @@ using Squadra.Server.Modules.Powiadomienia.Enums;
 using Squadra.Server.Modules.Powiadomienia.Repositories;
 using Squadra.Server.Modules.Profile.Services;
 using Squadra.Server.Modules.Shared.Services;
-using Squadra.Server.Modules.Uzytkownicy.Models;
+using Squadra.Server.Modules.Statystyki.Services;
 using Squadra.Server.Modules.Uzytkownicy.Services;
 using Squadra.Server.Modules.Znajomosci.Repositories;
 using Squadra.Server.Modules.Znajomosci.Services;
@@ -20,7 +18,7 @@ public class PowiadomienieService(IPowiadomienieRepository powiadomienieReposito
     IZnajomiService znajomiService,
     IZnajomiRepository znajomiRepository,
     IProfilService profilService,
-    IUsuwanieNadmiaruPowiadomienService usuwanieNadmiaruPowiadomienService,
+    IStatystykiService statystykiService,
     IDruzynyRepository druzynyRepository // importujemy repozytorium, aby nie powodować zapętlenia, a i tak jedyne co potrzebujemy to proste pobieranie z bazy
     ) : IPowiadomienieService
 {
@@ -166,7 +164,7 @@ public class PowiadomienieService(IPowiadomienieRepository powiadomienieReposito
         // jak tu dochodzimy, wszystko jest git
         await powiadomienieRepository.CreatePowiadomienie(powiadomienie);
         // usuwamy powiadomienia przekraczające limit
-        await usuwanieNadmiaruPowiadomienService.UsunNadmiarowePowiadomieniaUzytkownika(powiadomienie.IdUzytkownika);
+        await UsunNadmiarowePowiadomieniaUzytkownika(powiadomienie.IdUzytkownika);
         return ServiceResult<bool>.NoContent(true);
         
     }
@@ -201,6 +199,73 @@ public class PowiadomienieService(IPowiadomienieRepository powiadomienieReposito
         return ServiceResult<bool>.NoContent(await powiadomienieRepository.DeletePowiadomieniaDanegoTypuPowiazaneZObiektami(idUzytkownika, idTypu, idPowiazanegoObiektu, idDrugiegoPowiazanegoObiektu));
     }
     
+    public async Task<bool> UsunNadmiarowePowiadomieniaUzytkownika(int idUzytkownika)
+    {
+        var powiadomieniaDoUsuniecia = await powiadomienieRepository.PodajPowiadomieniaUzytkownikaPrzekraczajaceLimit(idUzytkownika);
+        if (powiadomieniaDoUsuniecia.Count == 0) return true;
+        // w przypadku usuwania zaproszeń musimy wysłać powiadomienie zwrotne, że zaproszenie zostało odrzucone
+        foreach (var powiadomienieDoUsuniecia in powiadomieniaDoUsuniecia)
+        {
+            if ((TypPowiadomieniaEnum)powiadomienieDoUsuniecia.TypPowiadomieniaId is TypPowiadomieniaEnum.ZaproszenieDoZnajomych)
+            {
+                var profilRes = await profilService.GetProfil(idUzytkownika);
+                if (!profilRes.Succeeded) continue; // coś jest nie tak z powiadomieniem, więc nie wysyłamy powiadomienia zwrotnego, tylko usuwamy
+                
+                // wysyłamy powiadomienie do zapraszającego, że jego zaproszenie zostało odrzucone, bo nie ma miejsca w powiadomieniach
+                var powiadomienieDoZapraszajacego = new PowiadomienieCreateDto(
+                    (int)TypPowiadomieniaEnum.OdrzucenieZaproszeniaDoZnajomych,
+                    powiadomienieDoUsuniecia.PowiazanyObiektId ?? 1, // nie będzie sytuacji tutaj, że to będzie null, ale aby się nie czepiał kompilator
+                    idUzytkownika, // powiązany jest użytkownik, u którego jest to usuwane
+                    profilRes.Value.Pseudonim,
+                    null,
+                    null,
+                    null
+                );
+                await powiadomienieRepository.CreatePowiadomienie(powiadomienieDoZapraszajacego); // nie uruchamiamy jeszcze raz w serwisie, bo to by mogło spowodować pętlę, więc wywołujemy repozytorium bezpośrednio
+            }
+
+            if ((TypPowiadomieniaEnum)powiadomienieDoUsuniecia.TypPowiadomieniaId is TypPowiadomieniaEnum.ZaproszenieDoDruzyny)
+            {
+                try
+                {
+                    var druzyna = await druzynyRepository.GetDruzyna(powiadomienieDoUsuniecia.PowiazanyObiektId ?? 1); // nie będzie sytuacji tutaj, że to będzie null, ale aby się nie czepiał kompilator
+
+                    // pobieramy miejsce w drużynie
+                    var miejsce = await druzynyRepository.GetMiejsceWDruzynie(powiadomienieDoUsuniecia.DrugiPowiazanyObiektId ?? 1);
+                    
+                    string? nazwaRoli = null;
+                    if (miejsce.RolaId != null)
+                    {
+                        var rolaRes = await statystykiService.GetRola(miejsce.RolaId ?? 1);
+                        if (rolaRes.Succeeded && rolaRes.Value != null) nazwaRoli = rolaRes.Value.Nazwa;
+                    }
+
+                    var profilRes = await profilService.GetProfil(idUzytkownika);
+                    if (!profilRes.Succeeded)
+                        continue;
+
+                    var powiadomienieDoKapitana = new PowiadomienieCreateDto(
+                        (int)TypPowiadomieniaEnum.OdrzucenieZaproszeniaDoDruzyny,
+                        druzyna.KapitanId,
+                        idUzytkownika, // powiązany jest użytkownik, u którego jest to usuwane
+                        profilRes.Value.Pseudonim,
+                        druzyna.Id,
+                        druzyna.Nazwa,
+                        nazwaRoli
+                    );
+                    
+                    // nie uruchamiamy jeszcze raz w serwisie, bo to by mogło spowodować pętlę, więc wywołujemy repozytorium bezpośrednio}
+                    await powiadomienieRepository.CreatePowiadomienie(powiadomienieDoKapitana);
+                }
+                catch (NieZnalezionoWBazieException e)
+                {
+                    continue; // coś jest nie tak z powiadomieniem, więc nie wysyłamy powiadomienia zwrotnego, tylko usuwamy
+                }
+            }
+        }
+        await powiadomienieRepository.UsunPowiadomienia(powiadomieniaDoUsuniecia);
+        return true;
+    }
 
         // najpierw zmieniamy login na id, potem wywołujemy zapraszanie po id
         public async Task<ServiceResult<bool>> WyslijZaproszenieDoZnajomychPoLoginie(int idZapraszajacego, string loginZaproszonego)
