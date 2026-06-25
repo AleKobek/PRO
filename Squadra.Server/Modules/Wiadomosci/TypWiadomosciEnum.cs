d8342d939d0a05d1e5fae5873891814754790ddb
AleKobek <ola.kobek@gmail.com>
2026-06-25 18:07:00 +0200

    feat(wiadomości): dodanie enuma typu wiadomości

---
+++ Squadra.Server/Modules/Wiadomosci/TypWiadomosciEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace Squadra.Server.Modules.Wiadomosci;
+
+public enum TypWiadomosciEnum
+{
+    Prywatna = 1, // wiadomość prywatna między dwoma użytkownikami
+    Druzynowa = 2 // wiadomość w drużynie, widoczna dla wszystkich członków drużyny
+}
